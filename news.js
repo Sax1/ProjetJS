e70a987ea660bb0bad716c6740d684ebaf8d6272
ROUSSEAU Sylvain <rousssyl@hostname.iut2.upmf-grenoble.fr>
2017-03-15 08:32:02 +0100

    init

---
+++ news.js
@@ -0,0 +1,54 @@
+var recherches=[];//tableau contenant des chaines de caracteres correspondant aux recherches stockees
+var recherche_courante;// chaine de caracteres correspondant a la recherche courante
+var recherche_courante_news=[]; // tableau d'objets de type resultats (avec titre, date et url)
+
+function ajouter_recherche()
+{
+	if(recherches.indexOf(zone_saisie) == -1){
+		recherches.pushback(zone_saisie)
+	}
+}
+
+function supprimer_recherche(e)
+{
+
+
+}
+
+
+function selectionner_recherche(e)
+{
+
+}
+
+
+function init()
+{
+
+}
+
+
+function recherche_nouvelles()
+{
+
+
+}
+
+
+function maj_resultats(res)
+{
+
+
+}
+
+
+function sauve_news(e)
+{
+
+}
+
+
+function supprime_news(e)
+{
+
+}
